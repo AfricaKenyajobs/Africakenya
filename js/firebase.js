b6cc64dbd7a8820b0f2da3721f05269a93d812e2
AfricaKenyajobs <mohamedkadra.08@gmail.com>
2026-04-24 12:50:32 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,35 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-// 🔥 PASTE YOUR COPIED CONFIG HERE
-const firebaseConfig = {
-  apiKey: "PASTE_HERE",
-  authDomain: "PASTE_HERE",
-  projectId: "PASTE_HERE",
-  storageBucket: "PASTE_HERE",
-  messagingSenderId: "PASTE_HERE",
-  appId: ""// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyASln99PrigGcoQDLXfa8mX3D7fzV-BK-M",
   authDomain: "africakenya-23155.firebaseapp.com",
   projectId: "africakenya-23155",
   storageBucket: "africakenya-23155.firebasestorage.app",
   messagingSenderId: "317714881079",
-  appId: "1:317714881079:web:eecf8ceb647d2de9e2bdb2",
-  measurementId: "G-6CJSSVY3MR"
+  appId: "1:317714881079:web:eecf8ceb647d2de9e2bdb2"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-};
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
